b2635994b9fdf7f14e7a7c740fbac1eb1be93f21
Alejandro Luis Figueroa Cevallos <figueroaaab07@gmail.com>
2022-06-15 18:08:43 -0700

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,39 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+// import { v4 as uuid } from "uuid";
+
+function App() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [urlDog, setUrlDog] = useState("");
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          // console.log(result.message);
+          setUrlDog(result.message);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <p>Loading...</p>;
+  } else {
+    return (
+      <div>
+        <img src={urlDog} alt="A Random Dog" />
+      </div>
+    );
+  }
+}
+
+export default App;
